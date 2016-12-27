2185e7eafb67c22decac1134bc510bd2c99352b3
jagub2 <jagub2@users.noreply.github.com>
2016-12-27 21:41:43 +0100

    add HWCR dataset

--- src/process_datasets.py
+++ src/process_datasets.py
@@ -5,8 +5,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import re
 import urllib.request
+import zipfile
 from scipy.misc import imread
-from src.settings import IMAGE_SIDE_PIXELS
+from src.settings import IMAGE_SIDE_PIXELS, DATASETS_DIR
 from src.image_utils import open_image, trim_image, resize_image, normalize_image
 
 
@@ -47,8 +48,7 @@ class ProcessUJIDataset(metaclass=abc.ABCMeta):
                     plt.scatter(x, y, s=0)
                     plt.plot(x, y, '-', c='b')
                 if letter.isupper() or letter.isdigit():
-                    image_filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "datasets",
-                                                  "%s-%s-%d.png" % (self.images_prefix, letter, i))
+                    image_filename = os.path.join(DATASETS_DIR, "%s-%s-%d.png" % (self.images_prefix, letter, i))
                     plt.savefig(os.path.join(image_filename), dpi=self.dpi)
                     image = open_image(image_filename)
                     image = trim_image(image)
@@ -62,12 +62,11 @@ class ProcessUJIDataset(metaclass=abc.ABCMeta):
         with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "datasets", out_filename), 'w') as \
                 out:
             out.write("%s;%s\n" % ("symbol", ';'.join(["data%d" % i for i in range(self.dpi ** 2)])))
-            for letter_file in glob.glob(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "datasets",
-                                                      "%s-*.png" % self.images_prefix)):
+            for letter_file in glob.glob(os.path.join(DATASETS_DIR, "%s-*.png" % self.images_prefix)):
                 letter = os.path.basename(letter_file).split('-')[1]
                 image = imread(letter_file, flatten=True)
                 image = normalize_image(image)
-                out.write("%s;%s\n" % (letter, ';'.join(['%.5f' % num for num in image])))
+                out.write("%s;%s\n" % (letter, ';'.join(['%.2f' % num for num in image])))
 
 
 class ProcessUJI1(ProcessUJIDataset):
@@ -78,20 +77,19 @@ class ProcessUJI1(ProcessUJIDataset):
         self.images_prefix = "UJI1"
 
     def is_dataset_present(self):
-        return [os.path.exists(file) for file in
-                [os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "datasets", "UJIpenchars-w%02d" % i)
+        return [os.path.exists(file) for file in [os.path.join(DATASETS_DIR, "UJIpenchars-w%02d" % i)
                  for i in self.persons]].count(False) == 0
 
     def acquire_dataset(self):
         for i in self.persons:
             urllib.request.urlretrieve(
                 "https://archive.ics.uci.edu/ml/machine-learning-databases/uji-penchars/version1/UJIpenchars-w%02d" % i,
-                os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "datasets", "UJIpenchars-w%02d" % i))
+                os.path.join(DATASETS_DIR, "UJIpenchars-w%02d" % i))
 
     def read_available_letters(self):
         letters_regex = re.compile(r'^.LEXICON')
         letter_regex = re.compile(r'"([a-zA-Z0-9])"')
-        with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "datasets", "UJIpenchars-w01"), 'r',
+        with open(os.path.join(DATASETS_DIR, "UJIpenchars-w01"), 'r',
                   encoding="utf8") as dataset_file:
             line = dataset_file.readline()
             if not line:
@@ -103,7 +101,7 @@ class ProcessUJI1(ProcessUJIDataset):
     def extract_letters(self):
         letter_regex = re.compile(r'.SEGMENT CHARACTER[^?]*\? "([a-zA-Z0-9])"')
         for person in self.persons:
-            with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "datasets", "UJIpenchars-w%02d" %
+            with open(os.path.join(DATASETS_DIR, "UJIpenchars-w%02d" %
                       person)) as person_letters:
                 line = person_letters.readline()
                 while True:
@@ -142,18 +140,16 @@ class ProcessUJI2(ProcessUJIDataset):
         self.images_prefix = "UJI2"
 
     def is_dataset_present(self):
-        return os.path.exists(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "datasets",
-                                           "ujipenchars2.txt"))
+        return os.path.exists(os.path.join(DATASETS_DIR, "ujipenchars2.txt"))
 
     def acquire_dataset(self):
         urllib.request.urlretrieve(
             "https://archive.ics.uci.edu/ml/machine-learning-databases/uji-penchars/version2/ujipenchars2.txt",
-            os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "datasets", "ujipenchars2.txt"))
+            os.path.join(DATASETS_DIR, "ujipenchars2.txt"))
 
     def read_available_letters(self):
         letters_regex = re.compile(r'// ASCII char: ([a-zA-Z0-9])')
-        with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "datasets", "ujipenchars2.txt"), 'r',
-                  encoding="utf8") as dataset_file:
+        with open(os.path.join(DATASETS_DIR, "ujipenchars2.txt"), 'r', encoding="utf8") as dataset_file:
             for line in dataset_file:
                 if letters_regex.search(line):
                     letter = letters_regex.findall(line)[0]
@@ -164,8 +160,7 @@ class ProcessUJI2(ProcessUJIDataset):
         word_regex = re.compile(r'^WORD ([a-zA-Z0-9])')
         curves_regex = re.compile(r'^\s+NUMSTROKES ([0-9]+)')
         points_regex = re.compile(r'^\s+POINTS ([0-9]+) # (.*)')
-        with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "datasets", "ujipenchars2.txt"), 'r',
-                  encoding="utf8") as dataset_file:
+        with open(os.path.join(DATASETS_DIR, "ujipenchars2.txt"), 'r', encoding="utf8") as dataset_file:
             for line in dataset_file:
                 if word_regex.search(line):
                     if word_regex.search(line):
@@ -186,12 +181,47 @@ class ProcessUJI2(ProcessUJIDataset):
                             self.data[letter] += [letter_curves]
 
 
+class ProcessHWCR(ProcessUJIDataset):
+    # source: https://github.com/sachinkariyattin/HWCR/tree/master/training_set/training_set
+    def __init__(self):
+        super(ProcessHWCR, self).__init__()
+        self.persons_count = 11
+        self.persons = range(1, self.persons_count + 1)
+        self.images_prefix = "HWCR"
+
+    def is_dataset_present(self):
+        return os.path.exists(os.path.join(DATASETS_DIR, "HWCR"))
+
+    def acquire_dataset(self):
+        with zipfile.ZipFile(os.path.join(DATASETS_DIR, "HWCR-letters.zip"), "r") as zip_ref:
+            zip_ref.extractall(DATASETS_DIR)
+
+    def read_available_letters(self):
+        pass
+
+    def extract_letters(self):
+        pass
+
+    def dump_letters(self):
+        i = 0
+        previous_letter = ''
+        for letter_file in os.listdir(os.path.join(DATASETS_DIR, "HWCR")):
+            filename = os.path.join(DATASETS_DIR, "HWCR", letter_file)
+            image = open_image(filename)
+            image = resize_image(image)
+            letter = letter_file[0].upper()
+            if previous_letter != letter:
+                previous_letter = letter
+                i = 0
+            image.save(os.path.join(DATASETS_DIR, "%s-%s-%d.png" % (self.images_prefix, letter, i)))
+            i += 1
+
+
 class ProcessDatasets(object):
     def __init__(self):
-        self.uji1 = ProcessUJI1()
-        self.uji2 = ProcessUJI2()
-        self.process_uji_dataset(self.uji1)
-        self.process_uji_dataset(self.uji2)
+        self.classes_to_process = [ProcessUJI1(), ProcessUJI2(), ProcessHWCR()]
+        for class_ in self.classes_to_process:
+            self.process_uji_dataset(class_)
 
     @staticmethod
     def process_uji_dataset(uji):
--- src/settings.py
+++ src/settings.py
@@ -3,6 +3,8 @@ from os import path, makedirs
 
 BASE_DIR = path.dirname(path.dirname(__file__))
 
+DATASETS_DIR = path.join(BASE_DIR, 'datasets')
+
 LOG_DIR = path.join(BASE_DIR, 'tensorflowlogs')
 
 if not path.exists(LOG_DIR):
